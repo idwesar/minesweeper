d7b6166df55f44ffd3a76ab42c600bbef580da00
idwesar <79699158+idwesar@users.noreply.github.com>
2021-05-16 14:19:12 +0100

    Visibility class - data & actions about cell visibility
    Stores data and actions about the visibility of cells

---
+++ visibility.py
@@ -0,0 +1,55 @@
+class Visibility:
+    def __init__(self):
+        #this will make the set that stores the visibility data. the coords for all the visible cells (i.e. everything that already)
+        self.viz = []
+
+    def isVisible(self, x, y):
+        #this will check the coords in the visibility set. if they're there, then the cell 'is visible'
+        if (x, y) in self.viz:
+            return True
+        else:
+            return False
+
+    def makeVisible(self, x, y):
+        #this will add the coords passed to it into the visibility set
+        self.viz.append((x, y))
+
+
+#unit testing
+def testVisibilityExists():
+    #arrange
+    visibility = Visibility()
+
+    x = 1
+    y = 3
+
+    #act
+    visibility.makeVisible(x, y)
+
+    #assert
+    result = visibility.isVisible(x, y)
+    assert(result)
+
+
+def testVisibilityNotExist():
+    #arrange
+    visibility = Visibility()
+
+    x = 2
+    y = 4
+
+    #act
+    visibility.makeVisible(x, y)
+
+    #assert
+    result = visibility.isVisible(x+1, y+1)
+    assert(not result)
+
+
+def main():
+    testVisibilityExists()
+    testVisibilityNotExist()
+    
+
+if __name__ == "__main__":
+    main()
