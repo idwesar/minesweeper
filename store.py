649e8a920f5fe3b542865d2693e730b34f301e27
idwesar <79699158+idwesar@users.noreply.github.com>
2021-06-07 22:21:23 +0100

    worked on unit testing

--- store.py
+++ store.py
@@ -13,6 +13,8 @@ class Store:
     def addMine(self, x, y):
         #add a mine to those coords
         self.mines.append((x, y))
+        
+        #add one to all the cells around the mine
         for xTemp in range(x-1, x+2):
             for yTemp in range(y-1, y+2):
                 if x != xTemp or y != yTemp: 
@@ -41,7 +43,23 @@ def testAdjacent():
     #assert
     assert(result == 0)
 
-#adjacentmines needs more testing - when the cell is a mine, when there are multiple mines, when there are no mines adjacent
+#adjacentmines needs more testing - when the cell is a mine, when there are multiple mines, when there are no mines adjacent#
+
+#when the cell is a mine
+#set a mine at certain coords
+#check those coords in the mine dict
+'''need to make sure that the counter dict is only checked if the coords arent a part of the mine dict'''
+#when you click on a mine its game over
+
+#when there are multiple mines
+#pick coords, set mines in the surrounding coords (alter the chosen coords)
+#check no of mines in counter (make sure to check in mines dict first because thats how its going to work)
+#should return a value above one
+
+#when there are no mines
+#dont set up any mines
+#check the mines
+#shoud tell you its not in the counter dict, i.e no mines nearby
 
 
 #testing adding a mine and checking the mine
